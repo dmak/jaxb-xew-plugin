c4c324996c8120a25df630376d27c4cfb0a0fcb1
Dmitry Katsubo <dmitry.katsubo@gmail.com>
2013-05-02 22:03:27 +0200

    Merge pull request #5 from wumpz/master

--- src/test/java/com/sun/tools/xjc/addon/xew/XmlElementWrapperPluginTest.java
+++ src/test/java/com/sun/tools/xjc/addon/xew/XmlElementWrapperPluginTest.java
@@ -1,47 +1,39 @@
 package com.sun.tools.xjc.addon.xew;
 
-import com.sun.tools.xjc.Driver;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
+
+import com.sun.tools.xjc.Driver;
+
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * Testcases for the XEW Plugin
+ * 
  * @author Tobias Warneke
  */
 public class XmlElementWrapperPluginTest {
 
-	@BeforeClass
-	public static void setUpClass() {
-	}
+	private static final String	PRECOMPILED_SOURCES_PREFIX	= "src/test/jaxb_resources/";
+	private static final String	GENERATED_SOURCES_PREFIX	= "target/test/generated-xsd-classes/";
 
-	@AfterClass
-	public static void tearDownClass() {
-	}
-
-	@Before
-	public void setUp() {
-	}
-
-	@After
-	public void tearDown() {
-	}
+	private static final Log	logger						= LogFactory.getLog(XmlElementWrapperPluginTest.class);
 
 	/**
 	 * Simple start of the available sample file with XEW.
 	 */
 	@Test
 	public void testSimpleStartTestWithXEW() throws Throwable {
-		assertEquals(2, assertXsd(this.getClass().getResource("sample.xsd").toString(), "sample_with_xew", true, "src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/sample_java", "Order.java", "ObjectFactory.java"));
+		assertXsd("sample.xsd", "sample_with_xew", true, 3, "Order.java", "ObjectFactory.java");
 	}
 
 	/**
@@ -49,53 +41,101 @@ public class XmlElementWrapperPluginTest {
 	 */
 	@Test
 	public void testSimpleStartTestWithOutXEW() throws Throwable {
-		assertEquals(3, assertXsd(this.getClass().getResource("sample.xsd").toString(), "sample_without_xew", false, "src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/sample_without_xew_java", "Order.java", "ObjectFactory.java", "Items.java"));
+		assertXsd("sample.xsd", "sample_without_xew", false, 5, "Order.java", "ObjectFactory.java", "Items.java");
 	}
-	
+
 	@Test
 	public void testSimpleSpecialCase() throws Throwable {
-		assertEquals(2, assertXsd(this.getClass().getResource("sample_specialcase.xsd").toString(), "sample_specialcase", true, "src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/sample_specialcase_java", "AXTest.java", "ObjectFactory.java"));
+		assertXsd("sample_specialcase.xsd", "sample_specialcase", true, 2, "AXTest.java", "ObjectFactory.java");
 	}
 
 	/**
-	 * Stanardtest for XSD examples.
-	 *
-	 * @param xsdUrl URL to xsd file
-	 * @param generateDir subfolder of target/test/generated-xsd-classes where
-	 * java - files are created
-	 * @param useXEWPlugin creating with or without XEW plugin
-	 * @param compareDir Exected sourcecodefiles. No content check will be done,
-	 * if this is null.
-	 * @param filesToCheck Expected files in generation dir. These files content
-	 * is checked, when compareDir is set.
+	 * Standard test for XSD examples.
+	 * 
+	 * @param xsdUrl
+	 *            URL to xsd file
+	 * @param testName
+	 *            subfolder of target/test/generated-xsd-classes where java - files are created
+	 * @param useXewPlugin
+	 *            creating with or without XEW plugin
+	 * @param totalFiles
+	 *            total number of generated files
+	 * @param filesToCheck
+	 *            Expected files in generation dir. These files content is checked, when compareDir is set.
 	 * @return
 	 * @throws Exception
 	 */
-	private int assertXsd(String xsdUrl, String generateDir, boolean useXEWPlugin, String compareDir, String... filesToCheck) throws Exception {
-		final File target = new File("target/test/generated-xsd-classes/" + generateDir);
+	private void assertXsd(String resourceXsd, String testName, boolean useXewPlugin, int totalFiles,
+				String... filesToCheck) throws Exception {
+		String xsdUrl = getClass().getResource(resourceXsd).getFile();
+
+		File target = new File(GENERATED_SOURCES_PREFIX + testName);
+
 		target.mkdirs();
 
-		if (useXEWPlugin) {
-			Driver.run(new String[]{"-verbose", "-no-header", "-Xxew", "-Xxew:instantiate lazy", "-Xxew:delete", "-d", target.getPath(), xsdUrl}, System.out, System.out);
-		} else {
-			Driver.run(new String[]{"-verbose", "-no-header", "-d", target.getPath(), xsdUrl}, System.out, System.out);
+		LoggingPrintStream loggingPrintStream = new LoggingPrintStream();
+
+		if (useXewPlugin) {
+			Driver.run(new String[]{"-verbose", "-no-header", "-Xxew", "-Xxew:instantiate lazy", "-Xxew:delete", "-d",
+					target.getPath(), xsdUrl}, loggingPrintStream, loggingPrintStream);
+		}
+		else {
+			Driver.run(new String[]{"-verbose", "-no-header", "-d", target.getPath(), xsdUrl}, loggingPrintStream,
+						loggingPrintStream);
 		}
 
 		String[] list = new File(target, "generated").list();
 
 		List<String> l = Arrays.asList(list);
 
+		assertEquals(totalFiles, list.length);
+
 		for (String ftoc : filesToCheck) {
 			assertTrue(l.contains(ftoc));
 		}
 
-		if (compareDir != null) {
-			for (String ftoc : filesToCheck) {
-				//To avoid Linefeed conflicts.
-				assertEquals(FileUtils.readLines(new File(compareDir, ftoc)),
+		String compareDir = PRECOMPILED_SOURCES_PREFIX + getClass().getPackage().getName().replace('.', '/') + "/"
+					+ testName;
+
+		for (String ftoc : filesToCheck) {
+			// To avoid CR/LF conflicts.
+			assertEquals(FileUtils.readLines(new File(compareDir, ftoc)),
 						FileUtils.readLines(new File(target, "generated/" + ftoc)));
+		}
+	}
+
+	/**
+	 * Class will redirect everything printed to {@link PrintStream} to logger.
+	 */
+	private static class LoggingPrintStream extends PrintStream {
+
+		private final StringBuilder	sb	= new StringBuilder();
+
+		public LoggingPrintStream() {
+			super(new NullOutputStream());
+		}
+
+		@Override
+		public void write(byte[] buf, int off, int len) {
+			for (int i = 0; i < len; i++) {
+				write(buf[off + i]);
 			}
 		}
-		return list.length;
+
+		@Override
+		public void write(int b) {
+			switch (b) {
+				case '\n' :
+					logger.debug(sb.toString());
+
+					sb.setLength(0);
+
+				case '\r' :
+					break;
+
+				default :
+					sb.append((char) b);
+			}
+		}
 	}
 }
