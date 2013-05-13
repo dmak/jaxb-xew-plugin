1a44338bd095149692492eb274d5d1e05d8e6a63
Dmitry Katsubo <dmitry.katsubo@gmail.com>
2013-05-13 16:32:16 +0200

    Improved Unit Tests / branch coverage.

--- src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/annotation_reference/ClassCommon.java
+++ src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/annotation_reference/ClassCommon.java
@@ -3,7 +3,6 @@ package annotation_reference;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlType;
 
@@ -18,8 +17,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="country" type="{http://www.w3.org/2001/XMLSchema}string"/>
- *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}byte"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -30,65 +28,29 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "class-common", propOrder = {
-    "country",
-    "code"
+    "id"
 })
 @XmlSeeAlso({
     ClassExt.class
 })
 public class ClassCommon {
 
-    @XmlElement(required = true)
-    protected String country;
-    @XmlElement(required = true)
-    protected String code;
+    protected byte id;
 
     /**
-     * Gets the value of the country property.
+     * Gets the value of the id property.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
      */
-    public String getCountry() {
-        return country;
+    public byte getId() {
+        return id;
     }
 
     /**
-     * Sets the value of the country property.
+     * Sets the value of the id property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
      */
-    public void setCountry(String value) {
-        this.country = value;
-    }
-
-    /**
-     * Gets the value of the code property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * Sets the value of the code property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setCode(String value) {
-        this.code = value;
+    public void setId(byte value) {
+        this.id = value;
     }
 
 }
--- src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/annotation_reference/ClassExt.java
+++ src/test/jaxb_resources/com/sun/tools/xjc/addon/xew/annotation_reference/ClassExt.java
@@ -17,7 +17,8 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;extension base="{}class-common">
  *       &lt;sequence>
- *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="country" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *       &lt;/sequence>
  *     &lt;/extension>
  *   &lt;/complexContent>
@@ -28,37 +29,64 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "class-ext", propOrder = {
-    "id"
+    "country",
+    "code"
 })
 public class ClassExt
     extends ClassCommon
 {
 
     @XmlElement(required = true)
-    protected String id;
+    protected String country;
+    @XmlElement(required = true)
+    protected String code;
 
     /**
-     * Gets the value of the id property.
+     * Gets the value of the country property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getId() {
-        return id;
+    public String getCountry() {
+        return country;
     }
 
     /**
-     * Sets the value of the id property.
+     * Sets the value of the country property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setId(String value) {
-        this.id = value;
+    public void setCountry(String value) {
+        this.country = value;
+    }
+
+    /**
+     * Gets the value of the code property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Sets the value of the code property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCode(String value) {
+        this.code = value;
     }
 
 }
